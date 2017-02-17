4b8c7670c617c4752498c630f81f98dffeba9d67
account931 <account931@ukr.net>
2017-02-17 12:47:21 +0200

    Add files via upload

---
+++ imageshop/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<META Name="copyright" Content="Me">
+<META Name="keywords" Content="Animated">
+<title>YESTERDAY_2_OCTOBER_2014</title>
+<link type="text/css" rel="stylesheet" href="style.css"/>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<link href="favicon.ico" rel="icon" type="image/x-icon" />
+</head>
+<body>
+<center>
+
+<H1 style="color:red;font-size:122px;">THIS PAGE IS SECURED</H!>
+
+
+</div>
+</center>
+
+
+</body></html>
